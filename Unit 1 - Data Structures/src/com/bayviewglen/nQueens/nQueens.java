f5f053527715b701f3be6665005ba92d2efdeeaa
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-09-23 23:05:46 -0400

    Added some lovely comments

--- Unit 1 - Data Structures/src/com/bayviewglen/nQueens/nQueens.java
+++ Unit 1 - Data Structures/src/com/bayviewglen/nQueens/nQueens.java
@@ -17,6 +17,7 @@ public class nQueens {
 			} catch (Exception e) {
 
 			}
+		}
 			qStack = new QueenArrayStack(n);
 			qStack.push(new Queen(0, 0));
 			boolean isBacktracking = false;
@@ -40,10 +41,22 @@ public class nQueens {
 				}
 			}
 
-		}
 		printBoard(qStack);
 
 	}
+	
+	/* Backtrack: 
+	 * 
+	 * This method is called when a Queen has a conflict, but has no more space to move on the x-axis 
+	 * (i.e when the Queen's x-position = n-1). Before the method is called, the Queen that caused the 
+	 * backtracking is popped from the stack, and filled (the controller variable for the columns) 
+	 * is reduced by 1, which is then passed into the method. The method then checks if the queen in 
+	 * the previous row has no conflicts and a space to move into -> if the previous Queen does have space, 
+	 * it shifts over a space in the x-axis. If there is no room for the Queen to move, the stack is popped
+	 * and the method calls itself until it finds a Queen that can move over, or the method returns -1, 
+	 * indicating that there is no solution to that particular board size. 
+	 * 
+	 */
 
 	private static int backtrack(int filled) {
 		if (!checkForConflict(qStack.peek()) && roomToSpare(qStack.peek(), n)) {
@@ -59,16 +72,42 @@ public class nQueens {
 		}
 		return filled;
 	}
+	
+	/* Shift Queen:
+	 *
+	 *When called, this method shifts a Queen over one row. 
+	 * 
+	 */
+	
+	private static void shiftQueen() {
+		qStack.peek().setxPos(qStack.peek().getxPos() + 1); // shifts queen one over in the x-axis
+	
+	}
 
+	/* Add:
+	 * 
+	 * This method adds a Queen to the stack at (0, filled). Filled is the controller variable 
+	 * for the current column the algorithm is working on. 
+	 * 
+	 */
+	
 	private static Stack addQueen(int filled) {
 		qStack.push(new Queen(0, filled));
 		return qStack;
 	}
-
-	private static void shiftQueen() {
-		qStack.peek().setxPos(qStack.peek().getxPos() + 1); // shifts queen one over in the x-axis
-
-	}
+	
+	/* Check for Conflict:
+	 * 
+	 * This method checks for conflicts between the topmost (current) Queen in the stack and the 
+	 * other Queens. It works by copying the current state of the stack to a new Stack object, so 
+	 * that operations can be performed on it, rather than possibly losing the data stored in the 
+	 * original stack object. It pops the top of the copied stack, to remove the current Queen. 
+	 * It then enters a for loop, checking whether each queen placed before it is either below it 
+	 * (by comparing y-values), or on a diagonal with it (by comparing the difference between the x-
+	 * and y-values of the two Queens being compared). If there if a conflict, the method returns true, 
+	 * otherwise it returns false. 
+	 * 
+	 */
 
 	private static boolean checkForConflict(Queen curr) {
 		Stack temp = qStack.clone();
@@ -91,10 +130,26 @@ public class nQueens {
 
 		return false;
 	}
+	
+	/*Room to Spare:
+	 * 
+	 * This method checks whether a Queen object has space to move over in the x-axis. It does so by checking 
+	 * whether the the current Queen's x-position plus one (the desired position) equals n. If a queen's x-position
+	 * was equal to n, it would be out of the bounds of the chess board. 
+	 * 
+	 */
 
 	private static boolean roomToSpare(Queen queen, int n) {
 		return (queen.getxPos() + 1 < n);
 	}
+	
+	/*Print Board:
+	 * 
+	 * This method prints the stack to the console. It first places every Queen by popping the stack and 
+	 * retrieving each Queen's unique x- and y-value. It then fills the rest of the spaces between the Queens
+	 * with an "X". 
+	 * 
+	 */
 
 	private static void printBoard(Stack qStack) {
 		String[][] board = new String[n][n];
