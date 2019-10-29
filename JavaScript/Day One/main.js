05c1ffa33b98c1fd229d803e842519958444c3d0
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-10-28 23:22:33 -0400

    Changed some stuff with vscode

--- JavaScript/Day One/main.js
+++ JavaScript/Day One/main.js
@@ -1,80 +1,66 @@
-function consoleTest(){
-//console.log("This \"is\" "); 
+function consoleTest() {
+	//console.log("This \"is\" "); 
 
-var x = 7; 
-//console.log(x); 
+	var x = 7;
+	//console.log(x); 
 
-x = 'changed the type, just like that!'; 
-//console.log(x); 
+	x = 'changed the type, just like that!';
+	//console.log(x); 
 
-var arr = []; 
+	var arr = [];
 
-arr[6] = 18; 
-arr[11] = "hello"; 
-//arr[20] = arr; 
+	arr[6] = 18;
+	arr[11] = "hi there!";
+	//arr[20] = arr; 
 
-arr[3] = function(){console.log("hi")}; 
+	arr[3] = function () {
+		console.log("hi")
+	};
 
-arr[3](); 
+	arr[3]();
 
-var obj = {}; 
-obj.color = "red"; 
-obj.height = 7; 
-obj['shoesize'] = 5; //same thing as saying the above
-obj.shoesize = 6; 
-obj[5] = 8; 
+	var obj = {};
+	obj.color = "red";
+	obj.height = 7;
+	obj['shoesize'] = 5; //same thing as saying the above
+	obj.shoesize = 6;
+	obj[5] = 8;
 
-obj.func1 = function(){alert(x)}; 
+	obj.func1 = function () {
+		alert(x)
+	};
 
-//console.log(obj); 
+	//console.log(obj); 
 
-//obj.func1(5); 
+	//obj.func1(5); 
 
-//arr.push(99); 
-//arr.pop(); 
-//arr.splice(6,8); 
-//var removedItems = arr.splice(6,1); 
+	//arr.push(99); 
+	//arr.pop(); 
+	//arr.splice(6,8); 
+	//var removedItems = arr.splice(6,1); 
 
-//iterator(removedItems); 
+	//iterator(removedItems); 
 
-mystery(1123, "seventy", "quack", false);
+	mystery(1123, "seventy", "quack", false);
 
-//console.log(obj); 
+	//console.log(obj); 
 
 
 }
 
-function iterator(coll){
-	for(var val in coll){
+function iterator(coll) {
+	for (var val in coll) {
 		//if(val !== undefined)
-			console.log('key: ' + val + ', value: ' + coll[val]);
+		console.log('key: ' + val + ', value: ' + coll[val]);
 	}
 }
 
-function mystery(a, b, c, d){
-	console.log(a); 
-	console.log(b); 
-	console.log(c); 
-	console.log(d); 
+function mystery(a, b, c, d) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+	console.log(d);
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
