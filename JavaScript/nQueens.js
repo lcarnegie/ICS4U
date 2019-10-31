a369d1dd1a57d84cc4d51f94b12ba6ec479b798e
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-10-30 22:53:51 -0400

    At least now you can input stuff

--- JavaScript/nQueens.js
+++ JavaScript/nQueens.js
@@ -1,99 +1,86 @@
-var qStack = [];
-var n;
-
 function takeInput() {
     var str = document.getElementById("numberInput").value;
     var isValid = false;
-    while (isValid == false) {
-        n = parseInt(str, 10);
-        if (n != NaN){
-            isValid = true; 
-        }else {
-            alert('Input invalid.')
-        }
-    } 
-   
-}
-
-function nQueens(nQueens){
-			qStack.push(new Queen(0, 0));
-			var isBacktracking = false;
-			for (var filled = 0; filled < n;) {
-				if (filled == -1) { 
-					console.log("There are no solutions.");
-					return;
-				} else if (checkForConflict(qStack.peek()) && !roomToSpare(qStack.peek(), n)) {
-					filled--;
-					qStack.pop();
-					filled = backtrack(filled);
-				} else if (checkForConflict(qStack.peek()) && roomToSpare(qStack.peek(), n)) {
-					shiftQueen();
-				} else if (!checkForConflict(qStack.peek())) {
-					filled++;
-					if (filled == n) {
-					} else {
-						qStack = addQueen(filled);
-					}
-				}
-			}
-
-		printBoard(qStack);
-}
-
-
-
-
-function shiftQueen() {
-    qStack.peek().setxPos(qStack.peek().getxPos() + 1);
-}
-
-function backtrack(filled) {
-    if (!checkForConflict(qStack.peek()) && roomToSpare(qStack.peek(), n)) {
-        shiftQueen();
+    var n = parseInt(str, 10);
+    if (isNaN(n)) {
+        alert("Input invalido")
     } else {
-        filled--;
+        nQueens(n);
+    }
+}
+
+function nQueens(numQueens) {
+    var qStack = [];
+    var n = numQueens;
+    qStack.push(new Queen(0, 0));
+    var isBacktracking = false;
+    for (var filled = 0; filled < n;) {
         if (filled == -1) {
-            return -1;
-        } else {
+            console.log("There are no solutions.");
+            return;
+        } else if (checkForConflict(qStack[qStack.length - 1]) && !roomToSpare(qStack[qStack.length - 1], n)) {
+            filled--;
             qStack.pop();
             filled = backtrack(filled);
-        }
-    }
-    return filled;
-}
-
-function checkForConflict(curr) {
-    var temp = qStack;
-    temp.pop(); // pops the current queen off of the stack
-    var conflict = false;
-    try {
-        while (!conflict) {
-            if (temp.peek().xPos === curr.getxPos() || Math.abs(temp.peek().xPos - curr.xPos) === Math.abs(temp.peek().yPos) - curr.yPos) {
-                conflict = true;
-                return true;
-            } else {
-                temp.pop();
+        } else if (checkForConflict(qStack[qStack.length - 1]) && roomToSpare(qStack[qStack.length - 1], n)) {
+            shiftQueen();
+        } else if (!checkForConflict(qStack[qStack.length - 1])) {
+            filled++;
+            if (filled == n) {} else {
+                addQueen(filled);
             }
         }
-    } catch (e) {
-        conflict = false;
+    }
+    printBoard(qStack);
+
+    function shiftQueen() {
+        qStack[qStack.length - 1].setxPos(qStack[qStack.length - 1].xPos + 1);
     }
 
-    return false;
+    function backtrack(filled) {
+        if (!checkForConflict(qStack[qStack.length - 1]) && roomToSpare(qStack[qStack.length - 1], n)) {
+            shiftQueen();
+        } else {
+            filled--;
+            if (filled == -1) {
+                return -1;
+            } else {
+                qStack.pop();
+                filled = backtrack(filled);
+            }
+        }
+        return filled;
+    }
+
+    function checkForConflict(curr) {
+        var temp = qStack.slice(0);
+        temp.pop(); // pops the current queen off of the stack
+        var conflict = false;
+        try {
+            while (!conflict) {
+                if (temp[temp.length - 1].xPos === curr.xPos || Math.abs(temp[temp.length - 1].xPos - curr.xPos) === Math.abs(temp[temp.length - 1].yPos) - curr.yPos) {
+                    conflict = true;
+                    return true;
+                } else {
+                    temp.pop();
+                }
+            }
+        } catch (e) {
+            conflict = false;
+        }
+
+        return false;
+    }
+
+
+    function roomToSpare(queen, n) {
+        return (queen.xPos + 1 < n);
+    }
+
+    function addQueen(filled) {
+		qStack.push(new Queen(0, filled));
+	}
 }
-
-
-function roomToSpare(queen, n) {
-    return (queen.xPos + 1 < n);
-}
-
-function printBoard(qStack) {
-    var stack = [
-        [5]
-    ];
-
-}
-
 class Queen {
     constructor(xPos, yPos) {
         this.xPos = xPos;
