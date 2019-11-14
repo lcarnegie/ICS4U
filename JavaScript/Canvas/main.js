bd776624c78c28cdc056e9c170193987655bc947
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-11-13 21:25:41 -0500

    did some work on random card maker

--- JavaScript/Canvas/main.js
+++ JavaScript/Canvas/main.js
@@ -13,9 +13,10 @@ function createImage(elem){
   var width = canvas.width;         // declares a variable called width and assigns it the width of the canvas
   var height = canvas.height;       // declares a variable called height and assigns it the height of the canvas
   
-  var octx = canvas.getContext("2d");
+  var oCtx = canvas.getContext("2d");
   currCard = cards["clubs5"]; 
-  octx.drawImage(currCard, 250, 100, currCard.width/2, currCard.height/2); 
+  oCtx.drawImage(currCard, 250, 100, currCard.width/4, currCard.height/4); 
+  
   /*
   var i = 10; 
   for (var c in cards){
--- JavaScript/Go Fish/goFish.js
+++ JavaScript/Go Fish/goFish.js
@@ -1,3 +1,4 @@
+var cards = {}; 
 function loadImages() {
     var suits = ['clubs', 'hearts', 'spades', 'diamonds',];
     var highCard = ['A', 'J', 'Q', 'K']; 
@@ -5,12 +6,45 @@ function loadImages() {
         var suit = c;
         for (var i = 2; i <= 10; i++) {
             var img = new Image();
-            img.src = "images/cards/clubs" + i + ".png";
+            img.src = "images/cards/" + suit + i + ".png";
             cards[suit + i] = img;
         }
-        for (var t of highCard){
-            
+        for(var h of highCard){
+            var img = new Image(); 
+            img.src = "images/cards/" + suit + h + ".png";
+            cards[suit + h] = img; 
         }
     }
 
+}
+
+function displayRandomCard(canvas){
+    var canvas = document.getElementById(canvas);   
+    var width = canvas.width;         
+    var height = canvas.height; 
+    var ctx = canvas.getContext("2d");
+    var randomCard = cards[randomCard()]; 
+    ctx.drawImage(randomCard, 250, 250, randomCard.width/4, randomCard.height/4); 
+}
+
+function randomCard(){
+    var suits = ['clubs', 'hearts', 'spades', 'diamonds'];
+    var randomSuit = suits[(Math.floor(Math.random() * 3))]; 
+    var randomNumber = (Math.floor(Math.random() * 14) + 1); 
+    if(randomNumber > 10){
+        if(randomNumber === 11){
+            randomNumber = "J"; 
+        }
+        if(randomNumber === 12){
+            randomNumber = "Q"; 
+        }
+        if(randomNumber === 13){
+            randomNumber = "K"; 
+        }
+        if(randomNumber === 14){
+            randomNumber= "A"; 
+        }
+    }
+    var image = randomSuit + randomNumber; 
+    return image;
 }
