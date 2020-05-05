6eaea78e99b385f08bf0bec957ab1add1eccf809
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2020-05-04 20:10:25 -0400

    added some things.. will probably change everything later

---
+++ AP CSP Create Project/main.js
@@ -0,0 +1,18 @@
+function Question(image, answer1, answer2, answer3, correctAnswer){
+    this.image = image; 
+    this.answer1 = answer1; 
+    this.answer2 = answer2; 
+    this.answer3 = answer3; 
+    this.correctAnswer = correctAnswer; 
+}
+
+let points = 0; 
+
+function startQuiz() {
+    let questions = initQuestions(); 
+
+}
+
+functions initQuestions() {
+
+}
