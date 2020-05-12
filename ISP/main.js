49115ced5cc01bc31df214e64e0d60c86a0ac21c
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2020-05-12 19:16:27 -0400

    Did a JS Test and started work on culminating task

---
+++ ISP/main.js
@@ -0,0 +1,122 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let scale = 1; // TO DO: Fix scaling and zooming in and out
+let x = 300;
+let y = 100;
+let dx = 20 * Math.pow(scale, -1); //speed of circle being drawn (or how many pixels are being added to x every time you draw the circle again) 
+let dy = 20 * Math.pow(scale, -1);
+let radius = 20 * scale;
+let stop = false;
+
+document.getElementById('stop').disabled = true;
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.beginPath();
+ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+ctx.fillStyle = 'orange';
+ctx.fill();
+
+
+function animate() {
+    let animation = requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+
+    if (stop) {
+        cancelAnimationFrame(animation);
+    }
+
+    if (x >= canvas.width - radius || x <= 0 + radius) {
+        dx = -dx; // change the sign of the velocity 
+    }
+
+    if (y >= canvas.height - radius || y <= 0 + radius) {
+        dy = -dy; // change the sign of the velocity 
+    }
+
+    x += dx;
+    y += dy;
+
+}
+
+
+
+function stopBall() {
+    document.getElementById('go').disabled = false;
+    document.getElementById('stop').disabled = true;
+    stop = true;
+}
+
+function moveBall() {
+    document.getElementById('stop').disabled = false;
+    document.getElementById('go').disabled = true;
+    stop = false;
+    animate();
+}
+
+function increaseSpeed() {
+    if (dx === 0) {
+        dx = 1;
+    }
+    if (dy === 0) {
+        dy = 1;
+    }
+    if (dx < 0) {
+        dx -= 20
+    }
+    if (dy < 0) {
+        dx -= 20
+    }
+    if (dx > 0) {
+        dx += 20
+    }
+    if (dy > 0) {
+        dx += 20
+    }
+}
+
+function decreaseSpeed() {
+    if (dx < 0) {
+        dx += 20
+    }
+    if (dy < 0) {
+        dx += 20
+    }
+    if (dx > 0) {
+        dx -= 20
+    }
+    if (dy > 0) {
+        dx -= 20
+    }
+}
+
+function zoomIn() {
+    scale += 0.5;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+}
+
+function zoomOut() {
+    scale -= 0.5;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+}
+
+
+
+
+
+
+
+
+
--- JavaScript and Web Development/Modern JS Test/test.js
+++ JavaScript and Web Development/Modern JS Test/test.js
@@ -1,78 +1,78 @@
 // Questions 1 and 2: 
 
 class Item {
-    constructor(name, sku, listprice, saleprice, quant){
-        this.name = name; 
-        this.sku = sku; 
-        this.listprice = listprice; 
-        this.saleprice = saleprice; 
-        this.quant = quant; 
-        this.total = saleprice * quant; 
+    constructor(name, sku, listprice, saleprice, quant) {
+        this.name = name;
+        this.sku = sku;
+        this.listprice = listprice;
+        this.saleprice = saleprice;
+        this.quant = quant;
+        this.total = saleprice * quant;
     }
 }
 
 class Bill {
-    constructor(){
-        this.items = []; 
+    constructor() {
+        this.items = [];
     }
 
-    push(item){
-        this.items.push(item); 
+    push(item) {
+        this.items.push(item);
     }
 
-    subtotal(){
-      return this.items.reduce((total, current) => total + current, 0); 
+    subtotal() {
+        return this.items.reduce((total, current) => total + current, 0);
     }
 
-    hst(){
-        return 0.13 * subtotal(); 
+    hst() {
+        return 0.13 * subtotal();
     }
 
-    total(){
-        return subtotal() + hst(); 
+    total() {
+        return subtotal() + hst();
     }
 
-    sale(){
-        return items.filter(item => item.saleprice <= (item.listprice - item.listprice * 0.25)); 
+    sale() {
+        return items.filter(item => item.saleprice <= (item.listprice - item.listprice * 0.25));
     }
 
-    sortItems(){
+    sortItems() {
         return this.items.sort((a, b) => {
-            if(a.name.charAt(0) > b.name.charAt(0)){
-                return 1; 
+            if (a.name.charAt(0) > b.name.charAt(0)) {
+                return 1;
             } else {
-                return -1; 
+                return -1;
             }
-        }); 
+        });
     }
 }
 
 
 // Question 3: 
 
-async function fetchUsers(){
-    const res = await fetch('https://jsonplaceholder.typicode.com/users'); 
-    const data = await res.json(); 
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
     data.sort((a, b) => {
-        if(a.email.charAt(0) > b.email.charAt(0)){
-            return 1; 
+        if (a.email.charAt(0) > b.email.charAt(0)) {
+            return 1;
         } else {
-            return -1; 
+            return -1;
         }
-    }); 
+    });
 
-    console.log(data); 
+    console.log(data);
 }
 
-fetchUsers(); 
+fetchUsers();
 
 
 // Question 4: 
 
-const birthYear = [1975, 1997, 2002, 1995, 1985]; 
+const birthYear = [1975, 1997, 2002, 1995, 1985];
 
-function getAges(){
-    return birthYear.map(birthYear => 2020 - birthYear); 
+function getAges() {
+    return birthYear.map(birthYear => 2020 - birthYear);
 }
 
 
